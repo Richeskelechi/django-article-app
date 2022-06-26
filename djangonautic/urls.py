96e667969ec61e955c79762f119436210e7d3524
Riches <richeskelechi94.rk@gmail.com>
2022-06-26 22:08:09 +0100

    Redirect fixed

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,20 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from .import views
 from django.conf.urls.static import static
 from django.conf import settings
 from articles import views as article_views
 from django.views.static import serve
-from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
     path('about/', views.about),
     path('', article_views.article_list, name="home"),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^assets/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^assets/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
